7f85f0bfd63ffce6edfda9fddfdd9d3cb4564875
Selim Yay <selimyay@MacBook-Air.local>
2026-06-24 14:04:50 +0300

    common tuples methods

--- tuples.py
+++ tuples.py
@@ -16,3 +16,17 @@ developer = ('Alice', 34, 'Rust Developer')
 name, *rest = developer
 print(name) # 'Alice'
 print(rest) # [34, 'Rust Developer']
+
+# count(): This method is used to determine how many times an item appears in a tuple.
+
+programming_languages1 = ('Rust', 'Java', 'Python', 'C++', 'Rust')
+print(programming_languages1.count('Rust')) # 2
+
+# index(): This method is used to find the index where a particular item is present in a tuple. 
+
+print(programming_languages1.index('Java')) # 1
+
+numbers = (13, 2, 78, 3, 45, 67, 18, 7)
+sorted(numbers) # [2, 3, 7, 13, 18, 45, 67, 78]
+# The sorted() function will always create a new list of the sorted values. This differs from the sort() method which sorts the elements of a list in place and does not return a new list.
+
